d21c780e0ada4d6e930cc15f5cf768ce2e66167c
walid <walidmostafa209@gmail.com>
2023-02-17 23:32:34 +0200

    edit code in user order page

--- cancel.php
+++ cancel.php
@@ -7,7 +7,7 @@ $conn = new PDO('mysql:host=localhost;dbname=coffee_shop', 'root', '');
 $status= "UPDATE orders_info SET status='Cancel' WHERE order_id=$id";
    $sql=$conn->prepare($status);     
    $sql->execute();
-  header("Location:User_Order.php");
+  header("Location:User_Orders.php");
   die();
 
 ?>
