8cf515db880b0ed0e037a67fc5d4cb1ad8f8d59a
ShimaaMourad <shimaamoubark97@gmail.com>
2023-02-17 22:45:14 +0200

    add cancel page

---
+++ cancel.php
@@ -0,0 +1,13 @@
+<?php
+$id=$_GET['cancelid'];
+
+// echo $id;
+$conn = new PDO('mysql:host=localhost;dbname=coffee_shop', 'root', '');
+
+$status= "UPDATE orders_info SET status='Cancel' WHERE order_id=$id";
+   $sql=$conn->prepare($status);     
+   $sql->execute();
+  header("Location:User_Order.php");
+  die();
+
+?>
